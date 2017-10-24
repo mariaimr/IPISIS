e87d886e7fa44ecf9d3bc2d79994efd6843d4ace
root <mimr0694@gmail.com>
2017-10-23 20:46:30 -0500

    reporte

--- api/controllers/ReporteController.js
+++ api/controllers/ReporteController.js
@@ -4,82 +4,80 @@
  * @description :: Server-side logic for managing Reporte
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
- var variable;
-  const nodeExcel = require('excel-export');
+
+// const nodeExcel = require('excel-export');
 module.exports= {
   proyectosAsignados: function (req, res) {
-
-    let conf ={cols: '', rows:''};
-    let datos= [];
-    let row=[];
-    let filas= [];
-
-    conf.cols = [{
-        caption:'Grupo Mares',
-        captionStyleIndex: 1,
-        type:'string',
-        width:30
-      },{
-        caption:'Nombre Proyecto',
-        type:'string',
-        width:90
-    },{
-      caption:'Estudiantes',
-      type:'string',
-      width:30
-    },{
-      caption:'Correo',
-      type:'string',
-      width:30
-    },{
-      caption:'tutor',
-      type:'string',
-      width:30
-    },{
-      caption:'Correo',
-      type:'string',
-      width:30
-    },{
-      caption:'Estado',
-      type:'string',
-      width:30
-    },{
-      caption:'Observación',
-      type:'string',
-      width:200
-    },{
-      caption:'fecha de Aprobación',
-      type:'string',
-      width:30
-    }
-  ];
+  //
+  //   let conf ={cols: '', rows:''};
+  //   let datos= [];
+  //   let row=[];
+  //   let filas= [];
+  //
+  //   conf.cols = [{
+  //       caption:'Grupo Mares',
+  //       captionStyleIndex: 1,
+  //       type:'string',
+  //       width:30
+  //     },{
+  //       caption:'Nombre Proyecto',
+  //       type:'string',
+  //       width:90
+  //   },{
+  //     caption:'Estudiantes',
+  //     type:'string',
+  //     width:30
+  //   },{
+  //     caption:'Correo',
+  //     type:'string',
+  //     width:30
+  //   },{
+  //     caption:'tutor',
+  //     type:'string',
+  //     width:30
+  //   },{
+  //     caption:'Correo',
+  //     type:'string',
+  //     width:30
+  //   },{
+  //     caption:'Estado',
+  //     type:'string',
+  //     width:30
+  //   },{
+  //     caption:'Observación',
+  //     type:'string',
+  //     width:200
+  //   },{
+  //     caption:'fecha de Aprobación',
+  //     type:'string',
+  //     width:30
+  //   }
+  // ];
 
 let consulta = `SELECT DISTINCT p.grupo_mares, p.nombre AS 'proyecto', est.nombre AS 'estudiante' , est.correo AS 'estudiante_email', prof.nombre AS 'tutor', prof.correo AS 'tutor_email', hi.estado, hi.observacion, hi.fecha_actualizacion FROM proyecto p INNER JOIN inscripcion i ON p.inscripcion_id = i.id INNER JOIN historial_inscripcion hi ON hi.inscripcion_id = i.id AND hi.estado = :aceptada OR hi.estado = :rechazada INNER JOIN oferta o ON i.oferta_id = o.id INNER JOIN profesor prof ON prof.id = o.profesor_id INNER JOIN equipo eq ON i.equipo_codigo = eq.codigo INNER JOIN equipo_estudiante ee ON eq.codigo = ee.equipo_codigo INNER JOIN estudiante est ON ee.estudiante_id = est.id`;
 
 sequelize.query(consulta, { replacements: { aceptada: 'ACEPTADA' , rechazada: 'RECHAZADA'}, type: sequelize.QueryTypes.SELECT})
   .then(reporte => {
-    reporte.forEach(function(item) {
-      for (var clave in item){
-          if (item.hasOwnProperty(clave)) {
-            row.push(item[clave]);
-          }
-        }
-        filas.push(row);
-        row=[];
-  });
-
-
-    console.log(conf.rows);
-    variable = filas;
-
+    return res.json(reporte);
+  //   reporte.forEach(function(item) {
+  //     for (var clave in item){
+  //         if (item.hasOwnProperty(clave)) {
+  //           row.push(item[clave]);
+  //         }
+  //       }
+  //       filas.push(row);
+  //       row=[];
+  // });
+}).catch(err => {
+  return res.serverError(err);
 });
-//  conf.rows= variable.slice();
-  console.log(variable);
-  var result = nodeExcel.execute(conf);
 
-  res.setHeader('Content-Type', 'application/vnd.openxmlformats');
-  res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
-  res.end(result, 'binary');
+
+  // var result = nodeExcel.execute(conf);
+  //
+  // res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+  // res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
+  // res.end(result, 'binary');
 
   }
 }
